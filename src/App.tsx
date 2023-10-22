f4fb23182f86b2e451ef8d0459433d838a584031
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-10-22 16:31:59 -0400

    Feat: add reset styles, variables and header components with class notation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <>
-      <h1>Hello project</h1>
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <main className="main">Main</main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className="">Header</div>
+      </header>
+    );
+  }
+}
+
+export default Header;
