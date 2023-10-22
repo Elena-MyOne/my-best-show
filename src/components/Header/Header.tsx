77ffe8f16c9f399ddb163b4548a2697ea16bf599
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-10-22 17:13:11 -0400

    feat: add header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import './Header.scss';
+import style from './Header.module.scss';
+import { MdLocalMovies } from 'react-icons/md';
+import { BsSearch } from 'react-icons/bs';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <div className="">Header</div>
+      <header className={style.header}>
+        <div className={`${style.body} container`}>
+          <div className={style.logo}>
+            <MdLocalMovies />
+            <span className={style.text}>My Best Show</span>
+          </div>
+          <div className={style.search}>
+            <input type="text" className={style.input} placeholder="Search show..." />
+            <button className={style.button}>
+              <BsSearch />
+            </button>
+          </div>
+        </div>
       </header>
     );
   }
