7505f0c49a809e3604c673afa1ce82f5f71f6eb3
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-11-30 00:05:52 -0500

    refactor: fix warning in scss files and delete folder vite-based project

--- vite-based-files/src/constants/page.constants.ts
+++
@@ -1,3 +0,0 @@
-export const LAST_PAGE = 200;
-export const ITEMS_PER_PAGE = 200;
-export const DEFAULT_ITEMS_PER_PAGE = 20;
--- vite-based-files/src/utils/getSearchValueFromLocalStorage.ts
+++
@@ -1,3 +0,0 @@
-export const getSearchValueFromLocalStorage = () => {
-  return localStorage.getItem('TVShowSearch') ?? '';
-};
